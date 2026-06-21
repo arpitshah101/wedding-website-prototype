21be07db2182d332eca2780e939edeb5295aea73
arpitshah101 <arpitshah101@gmail.com>
2026-06-20 22:22:12 -0400

    Update navbar content & background

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,10 +1,11 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: 'Home', url: '/' },
-  { name: 'Products', url: '/products' },
-  { name: 'Services', url: '/services' },
-  { name: 'Blog', url: '/blog' },
-  { name: 'Contact', url: '/contact' },
+  { name: 'Our Story', url: '/products' },
+  { name: 'Things to do', url: '/services' },
+  { name: 'Dress Code', url: '/blog' },
+  { name: 'Q&A', url: '/contact' },
+  { name: 'Travel & Accommodation', url: '/contact' },
 ];
 // An array of links for footer
 const footerLinks = [
